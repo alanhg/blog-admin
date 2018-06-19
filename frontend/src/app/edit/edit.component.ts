a95f42d9d4d9200d76b696ccf69f13db8d354d32
alanhg <qianghe@kland.com.cn>
2018-06-19 22:15:24 +0800

    博客编辑状态下，实现自动保存后端功能

--- frontend/src/app/edit/edit.component.ts
+++ frontend/src/app/edit/edit.component.ts
@@ -4,11 +4,15 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {Subscription} from "rxjs/Subscription";
 import {ProgressBarService} from "../core/progress-bar.service";
 import {ModalDirective} from "ngx-bootstrap";
+import {Subject} from "rxjs/Subject";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/observable/combineLatest';
 
 declare let showdown: any;
 
 /**
  * 双屏显示博客原文及渲染后页面
+ *
  */
 @Component({
   selector: 'app-edit',
@@ -18,17 +22,23 @@ declare let showdown: any;
 export class EditComponent implements OnInit, OnDestroy {
 
   renderedCnt: string;
-  sourceCnt: string;
   converter = new showdown.Converter();
-  title: string;
   updating: Subscription;
   @ViewChild("successModal") successModal: ModalDirective;
+
+  title: string;
+  sourceCnt: string;
+
+  title$ = new Subject<string>();
+  sourceCnt$ = new Subject<string>();
 
   constructor(private apiService: ApiService,
               private route: ActivatedRoute,
               private router: Router,
               private progressBarService: ProgressBarService) {
-
+    Observable.combineLatest(this.title$, this.sourceCnt$).debounceTime(400).subscribe(res => {
+      this.savePost();
+    })
   }
 
   ngOnInit() {
@@ -39,7 +49,9 @@ export class EditComponent implements OnInit, OnDestroy {
     });
   }
 
-
+  /**
+   * 发布
+   */
   publish() {
     this.progressBarService.show(true);
     this.apiService.deploy().subscribe(res => {
@@ -48,15 +60,28 @@ export class EditComponent implements OnInit, OnDestroy {
     })
   }
 
+  /**
+   * 更新成功提示确认
+   */
   confirm() {
     this.successModal.hide();
     this.router.navigateByUrl("/posts");
   }
 
-
+  /**
+   * 更新渲染
+   */
   updateView() {
+    this.sourceCnt$.next(this.sourceCnt);
     this.renderedCnt = this.converter.makeHtml(this.sourceCnt);
-    this.updating = this.apiService.updatePost(this.title, this.sourceCnt).subscribe(res => {
+  }
+
+  /**
+   * 保存文章
+   */
+  savePost() {
+    this.updating = this.apiService.updatePost(this.route.snapshot.paramMap.get("title"), this.title, this.sourceCnt).subscribe(res => {
+      this.router.navigate(["/posts/", this.title]);
     });
   }
 
