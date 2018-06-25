7dfb9d76eca05a5c0a9b149b0547629f19ca852d
alanhg <qianghe@kland.com.cn>
2018-06-25 19:28:28 +0800

    update .eslintrc

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const process = require('child_process');
 
 router.get('/process', (req, res) => {
-    var result = process.execSync(`pwd`, {
-        cwd: `/Users/alan/GitHub/alanhg.github.io/source/_posts/`,
-        encoding: 'utf8'
-    });
-    console.log(__dirname);
-    res.json({data: result});
+	var result = process.execSync(`pwd`, {
+		cwd: `/Users/alan/GitHub/alanhg.github.io/source/_posts/`,
+		encoding: 'utf8'
+	});
+	console.log(__dirname);
+	res.json({data: result});
 });
 module.exports = router;
