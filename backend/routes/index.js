d20a834a32e6f01b762ebabe8e1386f979783f44
alanhg <qianghe@kland.com.cn>
2018-06-25 19:23:42 +0800

    add .eslintrc

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
-const apiRouter = require('./api');
-router.use('/api', apiRouter);
-const testRouter = require('./test');
-router.use('/test', testRouter);
+const apiRouter = require('./api')
+router.use('/api', apiRouter)
+const testRouter = require('./test')
+router.use('/test', testRouter)
 
-module.exports = router;
+module.exports = router
